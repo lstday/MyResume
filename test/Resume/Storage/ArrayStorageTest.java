a4498d3b85b937c8b90f87780a4c7e66022d02ac
lstday <sakhar.medovich@gmail.com>
2015-10-29 19:34:54 +0300

    Add junit

---
+++ test/Resume/Storage/ArrayStorageTest.java
@@ -0,0 +1,40 @@
+package Resume.Storage;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lstday
+ * 29.10.15.
+ */
+public class ArrayStorageTest {
+
+    @org.junit.Test
+    public void testClear() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSave() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLoad() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetAllSorted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSize() throws Exception {
+
+    }
+}
